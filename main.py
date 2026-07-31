66976fd1262769331fde47be30913ce0bb948b22
피우리 <piwoorii@gmail.com>
2026-07-31 13:43:09 +0900

    feat: 메뉴 입력 처리 기능 구현

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from statistics import multimode
+
+
 def show_menu():
     print("==============================")
     print("       🫯나만의 퀴즈 게임🫯")
@@ -11,8 +14,25 @@ def show_menu():
 
 
 def main():
-    show_menu()
+    while True:
+        show_menu()
+        menu = input("메뉴를 선택하세요: ")
 
+        if menu == "1":
+            print("퀴즈 풀기를 선택했습니다.")
+        elif menu == "2":
+            print("퀴즈 추가를 선택했습니다.")
+        elif menu == "3":
+            print("퀴즈 목록을 추가했습니다.")
+        elif menu == "4":
+            print("점수 확인을 선택했습니다.")
+        elif menu == "5":
+            print("프로그램을 종료합니다.")
+            break
+        else:
+            print("잘못된 입력입니다.")
+
+        print()
 
 if __name__ == "__main__":
     main()
